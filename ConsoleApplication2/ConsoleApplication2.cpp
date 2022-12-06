3cf9a3960c6d33b986d0b671a85940a1d4189132
Jakub Jez <kubajez02@gmail.com>
2022-12-06 02:44:08 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int const rozm = 10;
+int a[rozm];
+int k = 0;
+
+void generujListe() {
+    srand(time(NULL));
+    for (int i = 0; i < rozm; i++) {
+        a[i] = rand() % 100;
+    }
+}
+
+int main()
+{
+    generujListe();
+    do {
+        k = 0;
+        for (int i = 0; i < rozm-1; i++) {
+            if (a[i] > a[i + 1]) {
+                int p = a[i + 1]; 
+                a[i+1] = a[i];
+                a[i] = p;     
+                k++;
+            }
+            for (int i = 0; i < rozm; i++) {
+                cout << a[i] << " ";
+            }
+            cout << "\n";
+        }
+        cout << '\n';
+    } while (k > 0);
+    
+    cout << "\n";
+
+    for (int i = 0; i < rozm; i++) {
+        cout << a[i]<<" ";
+    }
+}
---
+++ ConsoleApplication2/code.cpp
@@ -0,0 +1,8 @@
+#include "nag.h"
+int licznik = 0;
+
+int generujListe(row,col) {
+	for (int i = 0; i < row; i++) {
+		cout << i;
+	}
+}
