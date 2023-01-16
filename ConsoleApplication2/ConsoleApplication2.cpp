94bf7e1431fda91b2472df73e24499fbd557ed2b
Jakub Jez <kubajez02@gmail.com>
2023-01-16 16:01:29 +0100

    commit

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,42 +1,23 @@
 ﻿#include <iostream>
-#include <iomanip>
-
 using namespace std;
-
-int const rozm = 10;
-int a[rozm];
-int k = 0;
-
-void generujListe() {
-    srand(time(NULL));
-    for (int i = 0; i < rozm; i++) {
-        a[i] = rand() % 100;
-    }
-}
 
 int main()
 {
-    generujListe();
-    do {
-        k = 0;
-        for (int i = 0; i < rozm-1; i++) {
-            if (a[i] > a[i + 1]) {
-                int p = a[i + 1]; 
-                a[i+1] = a[i];
-                a[i] = p;     
-                k++;
-            }
-            for (int i = 0; i < rozm; i++) {
-                cout << a[i] << " ";
-            }
-            cout << "\n";
-        }
-        cout << '\n';
-    } while (k > 0);
-    
-    cout << "\n";
+    int a[] = { 9,4,4,5,6,6,9,12 };
+    int lewo = 0;
+    int prawo = 1;
+    do{
+        int tmp = a[lewo];
+        a[lewo] = a[prawo]; 
+        a[prawo] = tmp;
+        lewo++;
+        prawo++;
+    } while (a[lewo] > a[prawo]);
 
-    for (int i = 0; i < rozm; i++) {
-        cout << a[i]<<" ";
+
+    for (int i = 0;i < 8;i++) {
+        cout << a[i] << " ";
     }
+
+    return 0;
 }
